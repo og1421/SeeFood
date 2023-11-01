0eb1b123dc8570ab9a27a9cf2862188a0db298cc
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-10-31 21:42:24 -0300

    Initial Commit

---
+++ SeeFood/SeeFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeFoodApp.swift
+//  SeeFood
+//
+//  Created by Orlando Moraes Martins on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SeeFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
